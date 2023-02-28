ca5b989e585c341967d441b8626183289889bbd9
olehkhalin <olehkhalin@gmail.com>
2023-02-28 14:23:21 -0500

    fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { Inter } from "next/font/google";
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 import Creator from "components/Creator";
@@ -6,11 +7,14 @@ import Header from "components/Header";
 import ToastWrapper from "components/ToastWrapper";
 import WalletGlobalProvider from "components/WalletGlobalProvider";
 
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter();
+
 import "../globals.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         <WalletGlobalProvider>
           <Header />
