2e3a2acf6ed25bea5efd3daa6c44dc13fb56f119
olehkhalin <olehkhalin@gmail.com>
2023-02-28 17:22:09 -0500

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from "react";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 import Creator from "components/Creator";
@@ -7,15 +9,55 @@ import Header from "components/Header";
 import ToastWrapper from "components/ToastWrapper";
 import WalletGlobalProvider from "components/WalletGlobalProvider";
 
-// If loading a variable font, you don't need to specify the font weight
+import "../globals.css";
+
 const inter = Inter({ subsets: ["latin"] });
 
-import "../globals.css";
+export const metadata: Metadata = {
+  title: "Solana network workaround | artkhalin labs",
+  description:
+    "Creative developer living in USA, co-founder of vigvam.app, former lead developer at mad.fish. Featured projects: vigvam.app, stakeme.io, spooky.artkhalin.com, ilonakcopywrite.com, young-mystic.com.",
+  openGraph: {
+    title: "Solana network workaround | artkhalin labs",
+    description:
+      "Creative developer living in USA, co-founder of vigvam.app, former lead developer at mad.fish. Featured projects: vigvam.app, stakeme.io, spooky.artkhalin.com, ilonakcopywrite.com, young-mystic.com.",
+    url: "https://nextjs.org",
+    siteName: "Solana network workaround | artkhalin labs",
+    images: [
+      {
+        url: "https://solana.artkhalin.comm/og_image.jpg",
+        width: 1440,
+        height: 860,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    site: "Solana network workaround | artkhalin labs",
+    creator: "@olehkhalin",
+    description:
+      "Creative developer living in USA, co-founder of vigvam.app, former lead developer at mad.fish. Featured projects: vigvam.app, stakeme.io, spooky.artkhalin.com, ilonakcopywrite.com, young-mystic.com.",
+    title: "Solana network workaround | artkhalin labs",
+    images: [
+      {
+        url: "https://solana.artkhalin.comm/og_image.jpg",
+        width: 1440,
+        height: 860,
+      },
+    ],
+  },
+  generator: "Next.js",
+  creator: "@olehkhalin",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className}>
       <body>
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
         <WalletGlobalProvider>
           <Header />
           {children}
